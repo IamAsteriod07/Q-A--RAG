a6a28145abd586725e4490662d7cf9be0aedac94
AsteriodMallick <mallickabhishek456@gmail.com>
2025-10-24 21:00:53 +0530

    Update import paths for langchain modules

--- app.py
+++ app.py
@@ -4,13 +4,13 @@ Provides CLI interface for running different components.
 """
 import streamlit as st
 from langchain_groq import ChatGroq
-from langchain.text_splitter import RecursiveCharacterTextSplitter
+from langchain_text_splitters import RecursiveCharacterTextSplitter
 from langchain.chains.combine_documents import create_stuff_documents_chain
 from langchain_core.prompts import ChatPromptTemplate
 from langchain.chains import create_retrieval_chain
 from langchain_community.vectorstores import FAISS
 from langchain_community.document_loaders import PyPDFDirectoryLoader
-from langchain.embeddings import HuggingFaceEmbeddings
+from langchain_community.embeddings import HuggingFaceEmbeddings
 from dotenv import load_dotenv
 import os
 import time
